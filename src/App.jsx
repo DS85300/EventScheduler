1b11ce238850fdab52e9658f99be45b0691c7f33
LukasF7889 <lukas_fritsch@hotmail.de>
2025-04-04 12:11:28 +0200

    Added protected route

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,33 @@
-import { useState } from 'react'
-import { Router,Route,Routes } from 'react-router-dom';
-import './index.css'
-import MainLayout from './components/Layout';
-import LogIn from './pages/LogIn';
-import CreateEntry from './pages/CreateEntry';
-import Home from './pages/Home';
-import EventDetails from './pages/EventDetails';
+import { useState } from "react";
+import { Router, Route, Routes, Navigate, Outlet } from "react-router-dom";
+import "./index.css";
+import MainLayout from "./components/Layout";
+import LogIn from "./pages/LogIn";
+import CreateEntry from "./pages/CreateEntry";
+import Home from "./pages/Home";
+import EventDetails from "./pages/EventDetails";
+import authCheck from "./utils/authCheck";
 
 function App() {
   const [someState, setSomeState] = useState(null);
 
+  const ProtectedLayout = () => {
+    const isLoggedIn = authCheck();
+    return isLoggedIn ? <Outlet /> : <Navigate to="/login" />;
+  };
+
   return (
     <>
-    <Routes>
-      <Route path="/" element={<MainLayout/>}>
-        <Route index element={<Home />} />
-        <Route path="/CreateEvent" element={<CreateEntry/>}/>
-        <Route path="/LogIn" element={<LogIn/>}/>
-        <Route path="/event/:id" element={<EventDetails/>}/>
-      </Route>
-    </Routes>
+      <Routes>
+        <Route path="/" element={<MainLayout />}>
+          <Route index element={<Home />} />
+          <Route path="protected" element={<ProtectedLayout />}>
+            <Route index element={<CreateEntry />} />
+          </Route>
+          <Route path="/LogIn" element={<LogIn />} />
+          <Route index path="/event/:id" element={<EventDetails />} />
+        </Route>
+      </Routes>
     </>
   );
 }
--- src/pages/LogIn.jsx
+++ src/pages/LogIn.jsx
@@ -2,65 +2,73 @@ import { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 
 const LogIn = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState(null);
+  const navigate = useNavigate();
 
-const [email,setEmail]= useState("");
-const [password,setPassword] = useState("");
-const [error,setError]= useState(null);
-const navigate  = useNavigate();
-
-
-const handleSubmit = async (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
-    console.log({email,password})
+    console.log({ email, password });
     try {
-        const response = await fetch("http://localhost:3001/api/auth/login", {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json",
-            },
-            body: JSON.stringify({email,password}),
-        });
-        if (!response.ok) throw new Error("Log In failed"||`HTTP ${response.status}`|| errorData.message);
-        const data = await response.json();
-        localStorage.setItem("api-token",data.token);
-        navigate("/createEvent");
-        
-        
+      const response = await fetch("http://localhost:3001/api/auth/login", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ email, password }),
+      });
+      if (!response.ok)
+        throw new Error(
+          "Log In failed" || `HTTP ${response.status}` || errorData.message
+        );
+      const data = await response.json();
+      localStorage.setItem("api-token", data.token);
+      navigate("/");
     } catch (error) {
-        setError(error.message);
-        console.error(error);
+      setError(error.message);
+      console.error(error);
     }
-};
+  };
 
+  return (
+    <>
+      <div className="bg-gradient-to-br from-[#79c3e0] to-[#2084ab] flex items-center justify-center h-screen">
+        <form
+          onSubmit={handleSubmit}
+          className="fieldset w-xs bg-base-200 border border-base-300 p-4 rounded-box"
+        >
+          <legend className="fieldset-legend">Login</legend>
 
-    return (
+          {error && <p className="text-red-500">{error}</p>}
 
-        <>
-        <div className="bg-gradient-to-br from-[#79c3e0] to-[#2084ab] flex items-center justify-center h-screen">
-        <form onSubmit={handleSubmit} className="fieldset w-xs bg-base-200 border border-base-300 p-4 rounded-box">
-  <legend className="fieldset-legend">Login</legend>
+          <label className="fieldset-label">Email</label>
+          <input
+            type="email"
+            className="input"
+            placeholder="Email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            required
+          />
 
-  {error && <p className="text-red-500">{error}</p>}
-  
-  <label className="fieldset-label">Email</label>
-  <input type="email" className="input" placeholder="Email" 
-    value={email}
-    onChange={(e)=> setEmail(e.target.value)} 
-    required
-  />
-  
-  <label className="fieldset-label">Password</label>
-  <input type="password" className="input" placeholder="Password"
-    value={password}
-    onChange={(e)=> setPassword(e.target.value)} 
-    required
-  />
-  
-  <button  type="submit" className="btn btn-neutral mt-4">Login</button>
-</form>
-        </div>
-        </>
-    );
+          <label className="fieldset-label">Password</label>
+          <input
+            type="password"
+            className="input"
+            placeholder="Password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required
+          />
+
+          <button type="submit" className="btn btn-neutral mt-4">
+            Login
+          </button>
+        </form>
+      </div>
+    </>
+  );
 };
 
 export default LogIn;
